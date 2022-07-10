518749d4553a6ef705dd15c11529b42331dffc39
Dastan Ozgeldi <ozgdastan@gmail.com>
2022-07-10 16:18:12 +0600

    feat(bot): format files

--- deploy-commands.js
+++ deploy-commands.js
@@ -4,14 +4,16 @@ const { REST } = require("@discordjs/rest");
 const { Routes } = require("discord-api-types/v9");
 const { clientId, guildId, token } = require("./config.json");
 
-const commands = []
-const commandsPath = path.join(__dirname, 'commands')
-const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'))
+const commands = [];
+const commandsPath = path.join(__dirname, "commands");
+const commandFiles = fs
+  .readdirSync(commandsPath)
+  .filter((file) => file.endsWith(".js"));
 
 for (const file of commandFiles) {
-  const filePath = path.join(commandsPath, file)
-  const command = require(filePath)
-  commands.push(command.data.toJSON())
+  const filePath = path.join(commandsPath, file);
+  const command = require(filePath);
+  commands.push(command.data.toJSON());
 }
 
 const rest = new REST({ version: "9" }).setToken(token);
--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -1,20 +1,20 @@
 module.exports = {
-    name: 'interactionCreate',
-    async execute(interaction) {
-        if (!interaction.isCommand()) return;
+  name: "interactionCreate",
+  async execute(interaction) {
+    if (!interaction.isCommand()) return;
 
-        const command = interaction.client.commands.get(interaction.commandName);
-      
-        if (!command) return;
-      
-        try {
-          await command.execute(interaction);
-        } catch (err) {
-          console.error(err);
-          await interaction.reply({
-            content: "There was an error while executing this command",
-            ephemeral: true,
-          });
-        }
+    const command = interaction.client.commands.get(interaction.commandName);
+
+    if (!command) return;
+
+    try {
+      await command.execute(interaction);
+    } catch (err) {
+      console.error(err);
+      await interaction.reply({
+        content: "There was an error while executing this command",
+        ephemeral: true,
+      });
     }
-}
+  },
+};
