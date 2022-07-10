7584a8d022974a3c3b8018adb6dbbeb015cf3c2f
Dastan Ozgeldi <ozgdastan@gmail.com>
2022-07-10 16:37:38 +0600

    feat(bot): set presence on ready

--- events/ready.js
+++ events/ready.js
@@ -2,6 +2,7 @@ module.exports = {
   name: "ready",
   once: true,
   execute(client) {
+    client.user.setActivity('quryltai', { type: 'LISTENING'})
     console.log(`Ready! Logged in as ${client.user.tag}`);
   },
 };
